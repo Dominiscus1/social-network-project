f0e2a293a3fa348c198ade40f98bfa2d37f7d638
Anthony Jimenez <tony6776a@gmail.com>
2022-01-10 16:31:13 -0500

    updated momdels and controllers added schemas

---
+++ Develop/models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new Schema(
+  {
+    // TODO: add thoughtText
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    // TODO: add createdAt
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    // TODO: add username
+    username: {
+      type: String,
+      required: true
+    },
+    // TODO: add reactions
+    reaction: [reactionSchema]
+  },
+  {
+    // TODO: Add toJSON option
+    toJSON: {
+      getters: true
+    },
+    id: false
+  },
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ Develop/models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    // TODO: create username field
+    username: {
+      type: String,
+      required: true
+    },
+    // TODO: create email field 
+    email: { //is this correct?
+      type: String,
+      required: true
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
